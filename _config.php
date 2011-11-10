4e24572d35778c27ef79518e420b73681234bc53
Gordon Anderson <gordon.b.anderson@gmail.com>
2011-11-10 10:55:29 +0700

    Config: OVerride path in dom sort so that photographs can be treated differently

--- _config.php
+++ _config.php
@@ -1,4 +1,7 @@
 <?php
 
+//DataObjectManager_Controller/dosort
+// cant override DOM JS, so override at the routing level
+Director::addRules(100, array('DataObjectManager_Controller/$Action/Photograph' => 'PhotographDataObject_Controller'));
 
 ?>
