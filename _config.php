214bd892b2f0a5d16b2301ef1f2d5dae094f1049
Gordon Anderson <gordon.b.anderson@gmail.com>
2011-12-28 10:46:32 +0700

    Config: Decorator for publish all photos - might move to a tab

--- _config.php
+++ _config.php
@@ -3,5 +3,8 @@
 //DataObjectManager_Controller/dosort
 // cant override DOM JS, so override at the routing level
 Director::addRules(100, array('DataObjectManager_Controller/$Action/Photograph' => 'PhotographDataObject_Controller'));
+Director::addRules(100, array('fbimport/$Action/$ID' => 'Gallery_Controller'));
 
+
+Object::add_extension('CMSMain', 'GalleryCMSActionDecorator');
 ?>
