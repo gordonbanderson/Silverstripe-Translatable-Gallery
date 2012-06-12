7a9dadfbd114d4319e4f137390ed49461f721432
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-06-12 11:24:04 +0700

    Config: Made Photograph sortable

--- _config.php
+++ _config.php
@@ -5,6 +5,7 @@
 Director::addRules(100, array('DataObjectManager_Controller/$Action/Photograph' => 'PhotographDataObject_Controller'));
 Director::addRules(100, array('fbimport/$Action/$ID' => 'Gallery_Controller'));
 
+SortableDataObject::add_sortable_classes(array('Photograph'));
 
 //Object::add_extension('CMSMain', 'GalleryCMSActionDecorator');
 ?>
